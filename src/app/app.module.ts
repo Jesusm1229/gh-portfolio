96b807f3ac17687c3bdbab16adbdccff1a8d472a
Jesus <48733708+Jesusm1229@users.noreply.github.com>
2021-12-29 14:34:57 -0400

    adding prerendering content during build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { PanelComponent } from './panel/panel.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
 
+import { TransferHttpCacheModule } from '@nguniversal/common';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +20,8 @@ import { OrganizationsComponent } from './organizations/organizations.component'
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    HttpClientModule
+    HttpClientModule,
+    TransferHttpCacheModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   username: 'Jesusm1229',
+  apiUrl: 'https://api.github.com'
 };
